5e371d530a12f5f967a1e2616ddb9903db8cf1b5
derapper4567 <ashakisonga2022@gmail.com>
2025-07-19 12:36:39 +0300

    initial

---
+++ main.py
@@ -0,0 +1,19 @@
+
+from fastapi import FastAPI
+
+one = FastAPI()
+
+# Create a "path operation decorator". This tells FastAPI that the function below
+# is responsible for handling requests that go to the path "/" with a GET method.
+@one.get("/")
+async def read_root():
+
+    return {"massage": "Hello"}
+
+@one.get("/about")
+async def read_root():
+
+    return {"name": "Asha Melius Kisonga!" ,
+            "Age" : 22, 
+            "Pofessional": "MLOps Engineer",
+            "Hobbies": "Coding"}
